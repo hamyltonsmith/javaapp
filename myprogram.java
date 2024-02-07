0981a5080ff5dc9e97c1bfa8c95947d67d544f7c
root <root@ip-172-31-1-30.ap-south-1.compute.internal>
2024-02-07 15:28:11 +0000

    first java programme committed

---
+++ myprogram.java
@@ -0,0 +1,12 @@
+class Array
+{
+	public static void main(String[] args) {
+		int n=5;
+		// System.out.println(args.length);
+		int a[]=new int[n];
+		
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+	}
+}
